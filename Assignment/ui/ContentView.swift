42863d4f612ae009eafe58322329694079b9f632
Rakshit Kumar <rakshitk204@gmail.com>
2025-09-26 12:54:21 +0530

    updated code

--- Assignment/ui/ContentView.swift
+++ Assignment/ui/ContentView.swift
@@ -1,44 +1,28 @@
-//
-//  ContentView.swift
-//  Assignment
-//
-//  Created by Kunal on 03/01/25.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    @State private var viewModel = ContentViewModel()
-    @State private var path: [DeviceData] = [] // Navigation path
+  
+    @StateObject private var viewModel = ContentViewModel()
+    @State private var path: [DeviceData] = []
 
     var body: some View {
         NavigationStack(path: $path) {
             Group {
                 if let computers = viewModel.data, !computers.isEmpty {
-                                    DevicesList(devices: computers) { selectedComputer in
-                                        path.append(selectedComputer)
-                                    }
+                    DevicesList(devices: computers) { selectedComputer in
+                        path.append(selectedComputer)
+                    }
                 } else {
                     ProgressView("Loading...")
-                    
                 }
             }
-            .onChange(of: viewModel.navigateDetail, {
-                let navigate = viewModel.navigateDetail
-                    path.append(navigate!)
-                })
             .navigationTitle("Devices")
-            .navigationDestination(for: DeviceData.self) { computers in
-                DetailView(device: computers)
+            .navigationDestination(for: DeviceData.self) { computer in
+                DetailView(device: computer)
             }
             .onAppear {
                 if viewModel.data?.isEmpty ?? true {
-                                    viewModel.fetchAPI()
-                                }
-                    .navigationTitle("Devices")
-                                .navigationDestination(for: DeviceData.self) { computer in
-                                    DetailView(device: computer)
-                                }
+                    viewModel.fetchAPI()
                 }
             }
         }
